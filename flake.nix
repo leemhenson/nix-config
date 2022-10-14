6d4837ab220267e29107ded0339a639589087e6a
Lee Henson <lee.m.henson@gmail.com>
2022-10-14 13:27:58 +0100

    splitting

--- flake.nix
+++ flake.nix
@@ -9,16 +9,25 @@
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs"; # ...
   };
-  
+
   # add the inputs declared above to the argument attribute set
   outputs = { self, nixpkgs, home-manager, darwin }: {
     darwinConfigurations."Deimos" = darwin.lib.darwinSystem {
       # you can have multiple darwinConfigurations per flake, one per hostname
 
       system = "x86_64-darwin"; # "x86_64-darwin" if you're using a pre M1 mac
-      modules = [ 
+      modules = [
         home-manager.darwinModules.home-manager
         ./hosts/Deimos/default.nix
+      ]; # will be important later
+    };
+    darwinConfigurations."Phobos" = darwin.lib.darwinSystem {
+      # you can have multiple darwinConfigurations per flake, one per hostname
+
+      system = "aarch64-darwin"; # "x86_64-darwin" if you're using a pre M1 mac
+      modules = [
+        home-manager.darwinModules.home-manager
+        ./hosts/Phobos/default.nix
       ]; # will be important later
     };
   };
