910fedfbec5a37aef785d0c7d80294800d7c6b13
Lee Henson <lee.m.henson@gmail.com>
2026-05-12 13:15:08 +0100

    deno

--- hosts/Phobos/default.nix
+++ hosts/Phobos/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, unstablePkgs, ... }:
+{ unstablePkgs, ... }:
 
 {
   users.users.leemhenson = {
@@ -68,87 +68,93 @@
     useUserPackages = true;
     backupFileExtension = "backup";
 
-    users.leemhenson = { pkgs, ... }: {
-      fonts.fontconfig.enable = true;
+    users.leemhenson =
+      { pkgs, ... }:
+      {
+        fonts.fontconfig.enable = true;
 
-      xdg = {
-        enable = true;
+        xdg = {
+          enable = true;
 
-        configFile."bat/config".source = ../../dotfiles/bat/config;
-        configFile."helix/config.toml".source = ../../dotfiles/helix/config.toml;
-        configFile."nix/nix.conf".source = ../../dotfiles/nix/nix.conf;
-        configFile."nvim/init.lua".source = ../../dotfiles/nvim/init.lua;
-        configFile."vscode/wrapper/code".source = ../../dotfiles/vscode/wrapper/code;
-        configFile."vscode-insiders/wrapper/code-insiders".source = ../../dotfiles/vscode-insiders/wrapper/code;
-        configFile."zed/keymap.json".source = ../../dotfiles/zed/keymap.json;
-        configFile."zed/settings.json".source = ../../dotfiles/zed/settings.json;
+          configFile."bat/config".source = ../../dotfiles/bat/config;
+          configFile."helix/config.toml".source = ../../dotfiles/helix/config.toml;
+          configFile."nix/nix.conf".source = ../../dotfiles/nix/nix.conf;
+          configFile."nvim/init.lua".source = ../../dotfiles/nvim/init.lua;
+          configFile."vscode/wrapper/code".source = ../../dotfiles/vscode/wrapper/code;
+          configFile."vscode-insiders/wrapper/code-insiders".source =
+            ../../dotfiles/vscode-insiders/wrapper/code;
+          configFile."zed/keymap.json".source = ../../dotfiles/zed/keymap.json;
+          configFile."zed/settings.json".source = ../../dotfiles/zed/settings.json;
+        };
+
+        home = {
+          file = pkgs.lib.mapAttrs' (name: _: {
+            name = ".claude/skills/${name}";
+            value = {
+              source = ../../dotfiles/claude/skills + "/${name}";
+            };
+          }) (builtins.readDir ../../dotfiles/claude/skills);
+
+          packages = with pkgs; [
+            bash
+            bat
+            btop
+            cargo
+            cmake
+            colima
+            curl
+            deno
+            docker-compose
+            exiftool
+            fd
+            ffmpeg
+            gawk
+            git-lfs
+            httpie
+            imagemagick
+            jdk17
+            jq
+            libwebp
+            lsd
+            lua-language-server # needed for lua language server in nvim
+            nil # needed for nix language server in neovim
+            nixfmt-rfc-style # formatter for Nix
+            nodejs-slim
+            nodePackages."@tailwindcss/language-server"
+            nodePackages.typescript-language-server
+            openssh
+            openssl
+            pgcli
+            readline
+            ripgrep
+            ruby
+            serie
+            sqlfluff # formatter/linter for SQL
+            stylua # formatter for Lua
+            tldr
+            tree-sitter # needed by nvim-treesitter to compile parsers
+            unstablePkgs.helix
+            vscode-langservers-extracted # provides vscode-eslint-language-server
+            watchman
+            wget
+            yarn
+            zig
+          ];
+
+          stateVersion = "22.05";
+        };
+
+        programs = {
+          difftastic = import ../../dotfiles/difftastic/difftastic.nix;
+          direnv = import ../../dotfiles/direnv/direnv.nix;
+          fzf = import ../../dotfiles/fzf/fzf.nix;
+          git = import ../../dotfiles/git/git.nix pkgs;
+          gpg.enable = true;
+          htop.enable = true;
+          man.enable = true;
+          ssh = import ../../dotfiles/ssh/ssh.nix;
+          zsh = import ../../dotfiles/zsh/zsh.nix pkgs;
+        };
       };
-
-      home = {
-        file = pkgs.lib.mapAttrs' (name: _: {
-          name = ".claude/skills/${name}";
-          value = { source = ../../dotfiles/claude/skills + "/${name}"; };
-        }) (builtins.readDir ../../dotfiles/claude/skills);
-
-        packages = with pkgs; [
-          bash
-          bat
-          btop
-          cargo
-          cmake
-          colima
-          curl
-          docker-compose
-          exiftool
-          fd
-          ffmpeg
-          gawk
-          git-lfs
-          httpie
-          imagemagick
-          jdk17
-          jq
-          libwebp
-          lsd
-          lua-language-server # needed for lua language server in nvim
-          nil # needed for nix language server in neovim
-          nixfmt-rfc-style                              # formatter for Nix
-          nodejs-slim
-          nodePackages."@tailwindcss/language-server"
-          nodePackages.typescript-language-server
-          openssh
-          openssl
-          pgcli
-          readline
-          ripgrep
-          ruby
-          serie
-          sqlfluff                                      # formatter/linter for SQL
-          stylua                                        # formatter for Lua
-          tldr
-          tree-sitter             # needed by nvim-treesitter to compile parsers
-          unstablePkgs.helix
-          vscode-langservers-extracted  # provides vscode-eslint-language-server
-          watchman
-          wget
-          yarn
-          zig
-        ];
-
-        stateVersion = "22.05";
-      };
-
-      programs = {
-        difftastic = import ../../dotfiles/difftastic/difftastic.nix;
-        direnv = import ../../dotfiles/direnv/direnv.nix;
-        fzf = import ../../dotfiles/fzf/fzf.nix;
-        git = import ../../dotfiles/git/git.nix pkgs;
-        gpg.enable = true;
-        htop.enable = true;
-        man.enable = true;
-        ssh = import ../../dotfiles/ssh/ssh.nix;
-        zsh = import ../../dotfiles/zsh/zsh.nix pkgs;
-      };
-    };
   };
 }
